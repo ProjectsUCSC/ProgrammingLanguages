76e8215a89bf9c7664d9535dc4cd9e8eb86b1ae9
avani1004 <avarora@ucsc.edu>
2017-06-11 23:48:33 -0700

    GO refactored

--- main/parser_go.go
+++ main/parser_go.go
@@ -3,18 +3,23 @@ package main
 import (
 	"fmt"
 	"os/exec"
-	"strings"
+     //	"strings"
 	//"sync"
 )
+func parser(regex string, text string) (out []byte,_ int){
+	command := "/Users/avaniarora/Desktop/ProgrammingLanguages/./parser" + " " + regex + " " + text
+	out, _ = exec.Command("sh", "-c",command).Output() //"/Users/avaniarora/Desktop/ProgrammingLanguages/./parser" + " " + regex + " " + text).Output()
 
-func parser(regex string, text string) (out string){
-
-	out, _ := exec.Command("sh", "-c", "./parser" + " " + regex + " " + text).Output()
-    return
+//	fmt.Printf("%s\n",out)
+	return
 }
 
 func main() {
 	
-	out = parser("a+bc", "aaabc")
-	fmt.Printf("%s\n\n", out)
+	output,status := parser("aa", "aaa")
+	if(status == 0){
+		fmt.Printf("Matched with %s\n",output)
+	} else{
+		fmt.Printf("No Match\n")
+	}
 }
